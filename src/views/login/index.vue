c23acc7997f08467ac25e8333a038286643b98ac
sohan singh <sohan.singh@unoapp.io>
2019-07-10 16:15:58 -0400

    login design implemented

---
+++ src/views/login/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <app-nav></app-nav>
+
+    <v-container>
+      <v-layout align-center justify-center depressed>
+        <v-flex xs12 sm10>
+          <h3>Email</h3>
+
+          <v-text-field single-line solo></v-text-field>
+        </v-flex>
+      </v-layout>
+
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm10>
+          <h3>Password</h3>
+
+          <v-text-field single-line solo></v-text-field>
+        </v-flex>
+      </v-layout>
+      <v-layout align-center justify-center>
+        <v-btn depressed class="green white--text" justify-center>login</v-btn>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import navbar from "./navbar.vue";
+export default {
+  components: {
+    "app-nav": navbar
+  }
+};
+</script>
+
+<style>
+</style>
+
---
+++ src/views/login/navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-toolbar color="#FAFAFA">
+    <v-reponsive>
+      <v-img
+        :src="require('@/assets/logo.png')"
+        contain
+        height="100px"
+        width="100px"
+        position="left"
+      />
+    </v-reponsive>
+    <v-toolbar-title>Survey Admin</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-icon>person</v-icon>
+      <v-btn flat>login</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
